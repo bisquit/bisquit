b058305c55eb7a0e3175548b2fcd78217ac4d778
shuheikikuchi <qu1dpr09u0@gmail.com>
2023-06-09 18:37:16 +0900

    chore: Add pre-commit hook and update install.sh

--- install.sh
+++ install.sh
@@ -50,3 +50,4 @@ link_file "${DOTFILES_ROOT}/git/.tigrc" "$HOME/.tigrc"
 # fish
 mkdir -p "$HOME/.config/fish/functions"
 link_file "${DOTFILES_ROOT}/fish/config.fish" "$HOME/.config/fish/config.fish"
+
