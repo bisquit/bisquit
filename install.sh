7d3083ab4bdc03eb15f3ad2db590b0e56a401bd2
shuheikikuchi <qu1dpr09u0@gmail.com>
2023-06-09 14:00:25 +0900

    refactor: Add link_file function to install.sh

--- install.sh
+++ install.sh
@@ -1,9 +1,19 @@
 #!/bin/bash
 
-CLONE_DIST="$HOME/dotfiles"
+CLONE_DIST="$HOME/bisquit"
 
 git clone https://github.com/bisquit/bisquit.git ${CLONE_DIST}
 
 DOTFILES_ROOT="${CLONE_DIST}/dotfiles"
 
-ln -s ${DOTFILES_ROOT}/git/.gitconfig ~/.gitconfig
+link_file () {
+  local src=$1 dist=$2
+
+  if [ -e "$dist" ]; then
+    mv "$dist" "${dist}.backup"
+  fi
+
+  ln -s "$src" "$dist"
+}
+
+link_file "${DOTFILES_ROOT}/git/.gitconfig" "~/.gitconfig"
